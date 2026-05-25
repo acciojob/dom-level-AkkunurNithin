a1f36f96a37435d0053d0abd797ac14c2ca684b1
AkkunurNithin <akkunurnithin1234@gmail.com>
2026-05-25 13:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const elm = Document.getElmentId(level);
+
+var level = 0;
+
+while (el){
+	level++
+	el = el.parentElement;
+}
+var el = elm.parenentElement(all);
+
+alert = ("The level of the element is: " +level);
